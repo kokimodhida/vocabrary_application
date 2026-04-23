08695018ed6ed374e00b9980a505904f514c8c94
kokimochida <k.m.motty0628@gmail.com>
2026-04-23 17:47:18 +0900

    first commit

---
+++ src/main/java/com/udemy/spring3item/SpringVocabularyApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring3item;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringVocabularyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringVocabularyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/spring3item/SpringVocabularyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring3item;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringVocabularyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
